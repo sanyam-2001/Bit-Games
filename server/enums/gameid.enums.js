943dfa9c980518599afa7961318d6011481a841c
arnav <arnavsehgal2001@gmail.com>
2025-04-03 00:53:46 +0530

    enum addition and lobby socket creation setup

---
+++ server/enums/gameid.enums.js
@@ -0,0 +1,4 @@
+export const GameId = {
+  NO_GAME: 0,
+  TIC_TAC_TOE: 1,
+};
--- server/handlers/lobbyHandler.js
+++ server/handlers/lobbyHandler.js
@@ -1,15 +1,43 @@
 import { timelog } from "../utils/LoggingUtils.js";
+import { SocketEvents as events } from "../enums/socketevents.enums.js";
+import { generateLobbyId } from "../utils/LobbyUtils.js";
+import Lobby from "../Models/Lobby.model.js";
+import Player from "../Models/Player.model.js";
+import { GameId } from "../enums/gameid.enums.js";
+import redisService from "../services/Redis.service.js";
+import { uuid } from "uuidv4";
+import SocketPayload from "../Models/SocketPayload.model.js";
 
 const handleJoinLobby = (socket, { name, lobbyId }) => {
-    timelog("In lobby", lobbyId);
+  timelog("In lobby", lobbyId);
 };
 
-const handleCreateLobby = (socket, { name }) => {
-    timelog("Created lobby In Lobby");
+const handleCreateLobby = async (socket, { name: username }) => {
+  timelog("Created lobby In Lobby");
+
+  try {
+    const lobbyId = generateLobbyId();
+    const player = new Player(uuid(), username);
+    const lobby = new Lobby(lobbyId, [player], player.id, GameId.NO_GAME);
+
+    const response = await redisService.set(lobbyId, JSON.stringify(lobby));
+
+    if (!response) {
+      throw Error("Lobby creation failed");
+    }
+
+    socket.emit(events.ENTER_LOBBY, new SocketPayload(true, null, { lobby }));
+  } catch (error) {
+    console.error(error);
+
+    socket.emit(
+      events.ENTER_LOBBY,
+      new SocketPayload(false, "Room Creation Failed :(", null)
+    );
+  }
 };
 
 export const registerLobbyHandlers = (socket) => {
-    socket.on('joinLobby', (data) => handleJoinLobby(socket, data));
-    socket.on('createLobby', (data) => handleCreateLobby(socket, data));
+  socket.on(events.JOIN_LOBBY, (data) => handleJoinLobby(socket, data));
+  socket.on(events.CREATE_LOBBY, (data) => handleCreateLobby(socket, data));
 };
-
