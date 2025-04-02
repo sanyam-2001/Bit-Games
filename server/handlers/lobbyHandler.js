e1ec131f690d6f479e8fd8a97179d864a01f7621
Sanyam <sanyambhaskar5@gmail.com>
2025-04-03 03:54:41 +0530

    hotfix: removed double stringify bug

--- server/handlers/lobbyHandler.js
+++ server/handlers/lobbyHandler.js
@@ -1,26 +1,30 @@
 import { timelog } from "../utils/LoggingUtils.js";
-import { SocketEvents as events } from "../enums/SocketEvents.enums.js";
+import { SocketEvents as events } from "../enums/SocketEvents.enum.js";
 import { generateLobbyId } from "../utils/LobbyUtils.js";
 import Lobby from "../Models/Lobby.model.js";
 import Player from "../Models/Player.model.js";
-import { GameId } from "../enums/GameId.enums.js";
+import { GameId } from "../enums/GameId.enum.js";
 import redisService from "../services/Redis.service.js";
 import { v4 as uuid } from "uuid";
 import SocketPayload from "../Models/SocketPayload.model.js";
 
-const handleJoinLobby = (socket, { name, lobbyId }) => {
-    timelog("In lobby", lobbyId);
+const handleJoinLobby = async (socket, { name, lobbyId }) => {
+    try {
+        const lobby = await redisService.get(lobbyId);
+        console.log(lobby);
+    }
+    catch (error) {
+        console.error(error);
+    }
 };
 
 const handleCreateLobby = async (socket, { name: username }) => {
-    timelog("Created lobby In Lobby");
-
     try {
         const lobbyId = generateLobbyId();
         const player = new Player(uuid(), username);
         const lobby = new Lobby(lobbyId, [player], player.id, GameId.NO_GAME);
 
-        const response = await redisService.set(lobbyId, JSON.stringify(lobby));
+        const response = await redisService.set(lobbyId, lobby);
 
         if (!response) {
             throw Error("Lobby creation failed");
