ffc039d1ec282cf37ea4e998a72042c2cebbcb9e
kishanagarwal101 <kishan.agarwal@gmail.com>
2025-04-07 23:19:03 +0530

    added small ui change to tic tac toe component

---
+++ client/src/components/Games/TicTacToe/TicTacToe.js
@@ -0,0 +1,12 @@
+import styles from './TicTacToe.module.css';
+
+const TicTacToe = () =>{
+
+    return (
+        <div className={styles.container}>
+            <h1>Tic tac Thoko</h1>
+        </div>
+    )
+};
+
+export default TicTacToe;
